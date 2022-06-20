628f8b0b8c7ce527aa3e393c2f2257fabb181ce4
unknown <sonali.sahani93@gmail.com>
2022-06-20 23:56:26 +0530

    changes from workspace

---
+++ src/test/java/qaclickacademy/RestAssuredTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+	
+	@Test
+	public void restTest()
+	{
+		System.out.println("rest automation test");
+	}
+
+	
+	
+	@Test
+	public void postmanTest()
+	
+	{
+		System.out.println("postman test");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void borwserAutomation()
+	{
+		System.out.println("Browser automation");
+	}
+
+	@Test
+	public void elementUI()
+	{
+		System.out.println("element in UI");
+	}
+}
